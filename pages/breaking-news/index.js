2597a2a177d86ce5829f87b85dae79100ba2a446
Kishan9896 <ckishan9896@gmail.com>
2024-06-06 17:54:45 +0530

    changes

--- pages/breaking-news/index.js
+++ pages/breaking-news/index.js
@@ -139,7 +139,7 @@ function Breakingnews({ nodeByUri, loading }) {
               </p>
             </div>
           </div>
-          <div className="w-full max-w-4xl mx-auto">
+          {/* <div className="w-full max-w-4xl mx-auto">
             <ExportedImage
               className="mb-2"
               priority={true}
@@ -182,6 +182,55 @@ function Breakingnews({ nodeByUri, loading }) {
               dangerouslySetInnerHTML={{
                 __html: nodeByUri !== undefined && nodeByUri?.posts?.nodes[1]?.content,
               }}
+            />
+          </div> */}
+          <div className="w-full max-w-4xl mx-auto">
+            <ExportedImage
+              className="mb-2"
+              priority={true}
+              src={Frame}
+              alt="vladimirputin"
+              style={{ width: "100%", height: "auto", maxHeight: "500px" }}
+            />
+            <p className="text-[10px] md:text-[12px] text-base font-bold text-red-800">
+              UKRAINE - RUSSIA WAR
+            </p>
+            <h5 className="text-[13px] md:text-[15px] text-black-900 font-bold">
+              Zelensky rules out Erdogan's idea of peace summit with Russia
+              during Turkey visit
+            </h5>
+            <p className="text-[8px] md:text-[10px] text-base font-bold text-gray-800 mb-4">
+              <span
+                className="text-[10px] md:text-[12px] font-extrabold mr-1"
+                style={{ color: "#40A6FB" }}
+              >
+                |
+              </span>
+              By
+              <span
+                className="font-extrabold ml-1"
+                style={{ color: "#40A6FB" }}
+              >
+                Linah Absteen
+                <span
+                  className="text-[20px] md:text-[25px] font-extrabold mx-1"
+                  style={{ color: "#40A6FB" }}
+                >
+                  .
+                </span>
+              </span>
+              6 MIN READ
+            </p>
+            <p className="text-[13px] md:text-[15px] text-base font-normal text-gray-600 mb-3">
+              President Vladimir Putin said Wednesday that Russia is ready to
+              use nuclear weapons if its sovereignty or independence is
+              threatened, issuing another blunt warning to the West.
+            </p>
+            <ExportedImage
+              className="mb-2"
+              priority={true}
+              src={Screenshot2024}
+              alt="Screenshot2024"
             />
           </div>
         </div>
